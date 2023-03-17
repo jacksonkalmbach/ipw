807062a515f3f7f01e689c6a208f21ef2a3bd7fb
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-03-16 22:04:47 -0400

    additional mobile sign up page styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import {
 
 import Button from "../button/button.component.jsx";
 import GoogleSignInBtn from "../button/google-signin-btn.jsx";
-import FormInput from "../form-input/form-input.jsx";
+// import FormInput from "../form-input/form-input.jsx";
 
 import'./sign-in.styles.scss';
 
@@ -25,6 +25,14 @@ const SignInForm = () => {
     console.log(`Hello ${displayName}`)
   }
 
+  const handleSignUpPromptClick = () => {
+    console.log('sign up clicked')
+  }
+
+  const handleForgotPassword = () => {
+    console.log('Forgot password clicked')
+  }
+
   return(
     <div className="signin-container">
       <h1 className="signin-title">Sign In</h1>
@@ -34,25 +42,36 @@ const SignInForm = () => {
         <label>password</label>
         <input />
       </form>
+      <span
+        className="forgot-password"
+        onClick={handleForgotPassword}
+      >Forgot Password?</span>
       <div className="social-media-signin-web">
         <Button>Sign In</Button>
         <GoogleSignInBtn
           onClick={handleGoogleSignIn}
         />
       </div>
+      <div className="signin-mobile-btn">
+        <Button>Sign In</Button>
+      </div>
       <div className="social-media-signin-mobile">
         <p>---- OR SIGN IN WITH ----</p>
         <div className="social-media-signin-mobile-button-container">
-          <button
+          <GoogleSignInBtn 
             onClick={handleGoogleSignIn}
-          >GOOG</button>
+          />
           <button>FB</button>
         </div>
       </div>
       <div>
         <p>
           Don't have an account?
-          <span className="sign-up-prompt"> Sign Up</span>
+          <span 
+            className="sign-up-prompt"
+            onClick={handleSignUpPromptClick}
+            > Sign Up
+            </span>
         </p>
       </div>
     </div>
